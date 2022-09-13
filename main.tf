0f2f368904cfb847df673ba749c23deaa9d32e8e
Sam Coppin <samcoppin@Sams-MacBook-Air.local>
2022-09-13 12:33:23 +0100

    update

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "instance" {
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
+    protocol    = "http"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
