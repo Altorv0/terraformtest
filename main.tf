2e3857b79ca2daf0b172d112e0de152e101403b2
Sam Coppin <samcoppin@Sams-MacBook-Air.local>
2022-09-13 13:52:43 +0100

    update

--- main.tf
+++ main.tf
@@ -1,12 +1,17 @@
 provider "aws" {
   region = "us-east-2"
 }
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -25,10 +30,14 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   tags = {
     Name = "terraform-example"
   }
+  output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+}
 }
