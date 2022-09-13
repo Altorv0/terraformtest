95ff46b183e5bd57d433407f8dc5a27bf67e425e
Sam Coppin <samcoppin@Sams-MacBook-Air.local>
2022-09-13 18:05:04 +0100

    update

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ variable "server_port" {
 }
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
-
   ingress {
     from_port   = var.server_port
     to_port     = var.server_port
@@ -22,26 +21,20 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-resource "aws_launch_configuration" "example" {
-  image_id        = "ami-0c55b159cbfafe1f0"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
-
+resource "aws_instance" "example" {
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
-}
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-
-  min_size = 2
-  max_size = 10
-
-  tag {
-    key                 = "Name"
-    value               = "terraform-asg-example"
-    propagate_at_launch = true
+  tags = {
+    Name = "terraform-example"
   }
+}
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
