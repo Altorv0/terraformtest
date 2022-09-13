be312530c62b1719d90bd08aaedfa2e62bf00c98
Sam Coppin <samcoppin@Sams-MacBook-Air.local>
2022-09-13 11:29:14 +0100

    update

--- main.tf
+++ main.tf
@@ -2,10 +2,17 @@ provider "aws" {
   region = "us-east-2"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-tags = {
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  tags = {
     Name = "terraform-example"
   }
 }
