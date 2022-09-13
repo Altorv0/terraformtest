18e5939d0529e0c05e5366df192d4dce49f50cae
Sam Coppin <samcoppin@Sams-MacBook-Air.local>
2022-09-13 12:29:52 +0100

    update

--- main.tf
+++ main.tf
@@ -11,9 +11,9 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port   = all
-    to_port     = al
-    protocol    = all
+    from_port   = -1
+    to_port     = -1
+    protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
