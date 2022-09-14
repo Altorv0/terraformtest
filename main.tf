04e4016cdb2c0511c8cbf7009248e523d90dda2f
Sam Coppin <samcoppin@Sams-MacBook-Air.local>
2022-09-14 10:16:53 +0100

    ALB1

--- main.tf
+++ main.tf
@@ -84,12 +84,6 @@ resource "aws_lb_listener" "http" {
     }
   }
 }
-resource "aws_lb" "example" {
-  name               = "terraform-asg-example"
-  load_balancer_type = "application"
-  subnets            = data.aws_subnet_ids.default.ids
-  security_groups    = [aws_security_group.alb.id]
-}
 resource "aws_lb_target_group" "asg" {
   name     = "terraform-asg-example"
   port     = var.server_port
